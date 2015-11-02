98aa6b492b4e9897bec29ffdce087e09efd0689f
Kael <i@kael.me>
2015-11-02 23:13:27 +0800

    1.0.1: adds test caes, fixes bugs

--- test/hashed-fs.js
+++ test/hashed-fs.js
@@ -1,4 +1,48 @@
 'use strict';
 
 var expect = require('chai').expect;
-var hashed_fs = require('../');
+var hashed = require('../');
+var node_path = require('path');
+
+var root = node_path.join(__dirname, 'fixtures');
+function file (f) {
+  return node_path.join(root, f);
+}
+
+var HASH_a = 'ce2e532';
+
+describe("fs.stat()", function(){
+  it("stat", function(done){
+    hashed().stat(file('dir/b.js'), function (err, stat, cached) {
+      expect(err).to.equal(null);
+      expect(stat.hash).to.equal(HASH_a);
+      done();
+    });
+  });
+
+  it("stat cache", function(done){
+    var h = hashed();
+    var f = file('dir/cache.js');
+    h.stat(f, function (err, stat, cached) {
+      expect(err).to.equal(null);
+      expect(cached).to.equal(false);
+      h.stat(f, function (err, stat, cached) {
+        expect(err).to.equal(null);
+        expect(cached).to.equal(true);
+        done();
+      })
+    });
+  });
+});
+
+
+describe("fs.readFile()", function(){
+  it("readFile", function(done){
+    hashed().readFile(file('dir/a.js'), function (err, content, hash) {
+      expect(err).to.equal(null);
+      expect(content.toString()).to.equal('// a');
+      expect(hash.hash).to.equal(HASH_a);
+      done();
+    });
+  });
+});
